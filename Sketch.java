c8ac8a4b0915383fff9421568fd9d2bf7df67174
John MacDonald <john.macdonald23@ycdsbk12.ca>
2021-12-10 14:21:55 -0500

    Calling Functions (John M)

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,40 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
-  public void settings() {
-	// put your size call here
-    size(400, 400);
-  }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
-  public void setup() {
-    background(210, 255, 173);
-  }
+    public void settings() {
+        size(400, 400);
+    }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    public void setup() {
+        background(51, 204, 255);
+    }
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
-  
-  // define other methods down here.
+    public void draw() {
+
+        strokeWeight(4);
+        stroke(0, 0, 0);
+        fill(117, 73, 41);
+        triangle(200, 100, 375, 240, 25, 240);
+
+        strokeWeight(3);
+        fill(222, 158, 113);
+        rect(25, 240, 350, 380);
+
+        strokeWeight(2);
+        fill(60, 189, 222);
+        rect(60, 250, 75, 75);
+
+        strokeWeight(2);
+        fill(117, 73, 41);
+        rect(150, 275, 100, 275);
+
+        fill(247, 237, 40);
+        ellipse(15, 50, 75, 90);
+
+        fill(102, 255, 102);
+        rect(0, 380, 400, 300);
+
+
+    }
 }
